85321fdaffb2e17baace160f1759d4476e94713b
Torinoth <65074268+Torinoth@users.noreply.github.com>
2023-04-17 07:03:32 +0900

    追加：管理者ページ

--- pmm/admin.py
+++ pmm/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from pmm.models import PlasticModel
+from pmm.models import PlasticModel, brand, scale, stock_history
 
-admin.site.register(PlasticModel)
+
+class PlasticModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'scale', 'brand', 'stock', 'description')
+
+
+class brandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+class scaleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+class stock_historyAdmin(admin.ModelAdmin):
+    list_display = ('model', 'quantity_before', 'quantity_after', 'date')
+
+
+admin.site.register(PlasticModel, PlasticModelAdmin)
+admin.site.register(brand, brandAdmin)
+admin.site.register(scale, scaleAdmin)
+admin.site.register(stock_history, stock_historyAdmin)
