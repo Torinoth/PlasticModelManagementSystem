2ce2055cbd433be474be544ba9644dd38fcbba33
Torinoth <65074268+Torinoth@users.noreply.github.com>
2024-05-20 23:15:10 +0900

    RESTフレームワークの追加と製品ビューセットの作成
    Djangoの設定ファイルにRESTフレームワークを追加しました。さらに、新たに製品用のビューセットとシリアライザを作成して、製品のCRUDリクエストを処理できるようにしました。新規作成したAPIは'/api/'エンドポイントで利用できます。

--- pmm/urls.py
+++ pmm/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
 from . import views
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('', views.home, name='home'),
--- pmm/views.py
+++ pmm/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
 def home(request):
