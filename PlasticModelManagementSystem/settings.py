cc1e0a57e6fac2c69e076281910ed36b45674ee1
Torinoth <65074268+Torinoth@users.noreply.github.com>
2024-06-19 00:33:53 +0900

    ER図を追加
    新規にER図を追加しました。ER図は全体的なデータベースの設計を視覚的に示し、データモデルの構造を理解するのに役立ちます。この図では、Product、Maker、Brandの3つのテーブルと、それぞれのテーブルのカラム情報を表現しています。

--- PlasticModelManagementSystem/settings.py
+++ PlasticModelManagementSystem/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pmm',
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +120,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',
+]
+
